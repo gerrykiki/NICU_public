27e92a57e374c7c52cb24edc1b4105e85befab2a
Gerry <gerry_pw_lin@wistron.com>
2019-10-31 15:18:47 +0800

    Start with create-react-app

---
+++ build/precache-manifest.469744e0a585bcf1bc5d33d8c487b760.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3a520a4089a0ee80955795110b8e2eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c61f076d8a52b001b59",
+    "url": "/static/css/2.65d0fb34.chunk.css"
+  },
+  {
+    "revision": "c281f755b8531a364bfe",
+    "url": "/static/css/main.4d18f7d7.chunk.css"
+  },
+  {
+    "revision": "0c61f076d8a52b001b59",
+    "url": "/static/js/2.38f900e8.chunk.js"
+  },
+  {
+    "revision": "c281f755b8531a364bfe",
+    "url": "/static/js/main.dbe796b9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "def74a6e24dd5c88dc072e2585015295",
+    "url": "/static/media/Chooseedit.def74a6e.svg"
+  },
+  {
+    "revision": "aa66ed958651e7527d72633d88a7fc7b",
+    "url": "/static/media/Control.aa66ed95.svg"
+  },
+  {
+    "revision": "71fe11ca25b30688149df5cfb313aab5",
+    "url": "/static/media/Display.71fe11ca.svg"
+  },
+  {
+    "revision": "9f078a9917e686e408d3717461bc1e35",
+    "url": "/static/media/Edit.9f078a99.svg"
+  },
+  {
+    "revision": "0467b7bb8339f6bb241597b360c3c2d1",
+    "url": "/static/media/Historyview.0467b7bb.svg"
+  },
+  {
+    "revision": "6c75f5a7b7c5272abf6309ce74cd93b6",
+    "url": "/static/media/Wardview.6c75f5a7.svg"
+  },
+  {
+    "revision": "870c0c5263187da618f48677edc9cca8",
+    "url": "/static/media/delete.870c0c52.svg"
+  },
+  {
+    "revision": "bc0482ca62df1eaf86dbf6b3ceb6be74",
+    "url": "/static/media/details.bc0482ca.svg"
+  },
+  {
+    "revision": "8fc6270010c31c37ea510175bf1f67ad",
+    "url": "/static/media/edit2.8fc62700.svg"
+  },
+  {
+    "revision": "6f94c891225be6cc541a555c8fc1f5c3",
+    "url": "/static/media/file1.6f94c891.svg"
+  },
+  {
+    "revision": "c003605a0380a01bc6edd5da1698eddb",
+    "url": "/static/media/file2.c003605a.svg"
+  },
+  {
+    "revision": "43b66bbabfabcb13b5b251d514a4916e",
+    "url": "/static/media/filter.43b66bba.svg"
+  },
+  {
+    "revision": "253b77570bccd2dd4824627ad919acc7",
+    "url": "/static/media/hospital.253b7757.png"
+  }
+]);
